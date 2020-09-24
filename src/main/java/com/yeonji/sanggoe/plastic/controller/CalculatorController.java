931ff31ada21d8509bdb913f04e42893fd11b64b
sanggoe <sanggoe0509@gmail.com>
2020-09-25 02:15:54 +0900

    survey pages, waiting page, result1 page start

--- src/main/java/com/yeonji/sanggoe/plastic/controller/CalculatorController.java
+++ src/main/java/com/yeonji/sanggoe/plastic/controller/CalculatorController.java
@@ -41,20 +41,44 @@ public class CalculatorController {
         info.setPlasticGlove(glove);
         info.setPlasticCleanner(clean1);
         model.addAttribute("name", info.getName());
-        model.addAttribute("pet1", pet1);
-        model.addAttribute("pet2", pet2);
-        model.addAttribute("contain", contain);
-        model.addAttribute("eggPlt", eggPlt);
-        model.addAttribute("glove", glove);
-        model.addAttribute("clean1", clean1);
+        model.addAttribute("page", info.getPage());
+
         return "/calculator/survey2";
     }
 
-    @PostMapping("/waiting")
-    public String waitingPage(Model model) {
-        info.setPage(4);
+    @PostMapping("/survey3")
+    public String confirmSurvey3(@RequestParam("shampoo") int shampoo, @RequestParam("toothBrush") int toothBrush,
+                                 @RequestParam("toothPaste") int toothPaste, @RequestParam("clean2") int clean2,
+                                 @RequestParam("cosmetic") int cosmetic, @RequestParam("deliveFood") int deliveFood, Model model) {
+        info.setPage(3);
+        info.setPlasticShampoo(shampoo);
+        info.setPlasticToothbrush(toothBrush);
+        info.setPlasticToothpaste(toothPaste);
+        info.setPlasticCleansing(clean2);
+        info.setPlasticCosmetics(cosmetic);
+        info.setPlasticOrderFood(deliveFood);
+
         model.addAttribute("name", info.getName());
         model.addAttribute("page", info.getPage());
+
+        return "/calculator/survey3";
+    }
+
+    @PostMapping("/waiting")
+    public String waitingPage(@RequestParam("spoon") int spoon, @RequestParam("straw") int straw,
+                              @RequestParam("cup") int cup, @RequestParam("bagBig") int bagBig,
+                              @RequestParam("bagMiddle") int bagMiddle, @RequestParam("bagSmall") int bagSmall, Model model) {
+        info.setPage(4);
+        info.setPlasticSpoon(spoon);
+        info.setPlasticStraw(straw);
+        info.setPlasticTakeoutCup(cup);
+        info.setPlasticBagBig(bagBig);
+        info.setPlasticBagMiddle(bagMiddle);
+        info.setPlasticBagSmall(bagSmall);
+
+        model.addAttribute("name", info.getName());
+        model.addAttribute("page", info.getPage());
+
         return "/calculator/waiting";
     }
 
