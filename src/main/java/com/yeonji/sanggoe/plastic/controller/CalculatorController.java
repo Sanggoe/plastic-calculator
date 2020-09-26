9dee0f806417f0aab3d78e8fd24cbd46abe5aba2
sanggoe <sanggoe0509@gmail.com>
2020-09-26 18:56:18 +0900

    edit how to handle the data

--- src/main/java/com/yeonji/sanggoe/plastic/controller/CalculatorController.java
+++ src/main/java/com/yeonji/sanggoe/plastic/controller/CalculatorController.java
@@ -20,70 +20,98 @@ public class CalculatorController {
         this.calculateService = calculateService;
     }
 
-    @GetMapping(value = "/calculator/survey1")
+    @GetMapping(value = "/survey1")
     public String confirmSurvey(@RequestParam("name") String name, Model model) {
-        info.setName(name);
-        info.setPage(1);
         model.addAttribute("name", name);
-        model.addAttribute("page", info.getPage());
         return "/calculator/survey1";
     }
 
     @PostMapping("/survey2")
-    public String confirmSurvey2(@RequestParam("pet1") int pet1, @RequestParam("pet2") int pet2,
+    public String confirmSurvey2(@RequestParam("name") String name, // before data
+                                 @RequestParam("pet1") int pet1, @RequestParam("pet2") int pet2,
                                  @RequestParam("contain") int contain, @RequestParam("eggPlt") int eggPlt,
                                  @RequestParam("glove") int glove, @RequestParam("clean1") int clean1, Model model) {
-        info.setPage(2);
-        info.setPlasticPetBigBottle(pet1);
-        info.setPlasticPetSmallBottle(pet2);
-        info.setPlasticContainer(contain);
-        info.setPlasticEggPlate(eggPlt);
-        info.setPlasticGlove(glove);
-        info.setPlasticCleanner(clean1);
-        model.addAttribute("name", info.getName());
-        model.addAttribute("page", info.getPage());
+        model.addAttribute("name", name);
+
+        model.addAttribute("pet1", pet1);
+        model.addAttribute("pet2", pet2);
+        model.addAttribute("contain", contain);
+        model.addAttribute("eggPlt", eggPlt);
+        model.addAttribute("glove", glove);
+        model.addAttribute("clean1", clean1);
 
         return "/calculator/survey2";
     }
 
     @PostMapping("/survey3")
-    public String confirmSurvey3(@RequestParam("shampoo") int shampoo, @RequestParam("toothBrush") int toothBrush,
+    public String confirmSurvey3(@RequestParam("name") String name,
+                                 @RequestParam("pet1") int pet1, @RequestParam("pet2") int pet2,
+                                 @RequestParam("contain") int contain, @RequestParam("eggPlt") int eggPlt,
+                                 @RequestParam("glove") int glove, @RequestParam("clean1") int clean1, // before data
+                                 @RequestParam("shampoo") int shampoo,
+                                 @RequestParam("toothBrush") int toothBrush,
                                  @RequestParam("toothPaste") int toothPaste, @RequestParam("clean2") int clean2,
                                  @RequestParam("cosmetic") int cosmetic, @RequestParam("deliveFood") int deliveFood, Model model) {
-        info.setPage(3);
-        info.setPlasticShampoo(shampoo);
-        info.setPlasticToothbrush(toothBrush);
-        info.setPlasticToothpaste(toothPaste);
-        info.setPlasticCleansing(clean2);
-        info.setPlasticCosmetics(cosmetic);
-        info.setPlasticOrderFood(deliveFood);
 
-        model.addAttribute("name", info.getName());
-        model.addAttribute("page", info.getPage());
+        model.addAttribute("name", name);
+
+        model.addAttribute("pet1", pet1);
+        model.addAttribute("pet2", pet2);
+        model.addAttribute("contain", contain);
+        model.addAttribute("eggPlt", eggPlt);
+        model.addAttribute("glove", glove);
+        model.addAttribute("clean1", clean1);
+
+        model.addAttribute("shampoo", shampoo);
+        model.addAttribute("toothBrush", toothBrush);
+        model.addAttribute("toothPaste", toothPaste);
+        model.addAttribute("clean2", clean2);
+        model.addAttribute("cosmetic", cosmetic);
+        model.addAttribute("deliveFood", deliveFood);
 
         return "/calculator/survey3";
     }
 
     @PostMapping("/waiting")
-    public String waitingPage(@RequestParam("spoon") int spoon, @RequestParam("straw") int straw,
+    public String waitingPage(@RequestParam("name") String name,
+                              @RequestParam("pet1") int pet1, @RequestParam("pet2") int pet2,
+                              @RequestParam("contain") int contain, @RequestParam("eggPlt") int eggPlt,
+                              @RequestParam("glove") int glove, @RequestParam("clean1") int clean1,
+                              @RequestParam("shampoo") int shampoo, @RequestParam("toothBrush") int toothBrush,
+                              @RequestParam("toothPaste") int toothPaste, @RequestParam("clean2") int clean2,
+                              @RequestParam("cosmetic") int cosmetic, @RequestParam("deliveFood") int deliveFood, // before data
+                              @RequestParam("spoon") int spoon, @RequestParam("straw") int straw,
                               @RequestParam("cup") int cup, @RequestParam("bagBig") int bagBig,
                               @RequestParam("bagMiddle") int bagMiddle, @RequestParam("bagSmall") int bagSmall, Model model) {
-        info.setPage(4);
-        info.setPlasticSpoon(spoon);
-        info.setPlasticStraw(straw);
-        info.setPlasticTakeoutCup(cup);
-        info.setPlasticBagBig(bagBig);
-        info.setPlasticBagMiddle(bagMiddle);
-        info.setPlasticBagSmall(bagSmall);
 
-        model.addAttribute("name", info.getName());
-        model.addAttribute("page", info.getPage());
+        model.addAttribute("name", name);
 
-        return "/calculator/waiting";
+        model.addAttribute("pet1", pet1);
+        model.addAttribute("pet2", pet2);
+        model.addAttribute("contain", contain);
+        model.addAttribute("eggPlt", eggPlt);
+        model.addAttribute("glove", glove);
+        model.addAttribute("clean1", clean1);
+
+        model.addAttribute("shampoo", shampoo);
+        model.addAttribute("toothBrush", toothBrush);
+        model.addAttribute("toothPaste", toothPaste);
+        model.addAttribute("clean2", clean2);
+        model.addAttribute("cosmetic", cosmetic);
+        model.addAttribute("deliveFood", deliveFood);
+
+        model.addAttribute("spoon", spoon);
+        model.addAttribute("straw", straw);
+        model.addAttribute("cup", cup);
+        model.addAttribute("bagBig", bagBig);
+        model.addAttribute("bagMiddle", bagMiddle);
+        model.addAttribute("bagSmall", bagSmall);
+
+        return "/result/waiting";
     }
 
     @PostMapping("/result1")
     public String showResult1() {
-        return "/calculator/result1";
+        return "/result/result1";
     }
 }
